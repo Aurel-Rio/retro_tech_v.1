946859e0b874a02458bda2b7369fb19225df7dab
debian-riozacki <debian-riozacki@riozacki-ms-7d18.home>
2023-02-20 11:08:56 +0100

    retro_tech_v.1

--- api/users.js
+++ api/users.js
@@ -1,7 +1,9 @@
-/*
+
+const User = require('../models/user');
 const express = require('express');
 const router = express.Router();
-const Joi = require('joi'); // bibliothèque de validation
+const Joi = require('joi'); 
+// bibliothèque de validation
 
 // Middleware pour valider les données utilisateur
 function validateUser(user) {
@@ -39,36 +41,6 @@ const createUser = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send('Une erreur est survenue lors de la création de l\'utilisateur.'); // Renvoyer une erreur en cas d'échec
-  }
-}
-
-module.exports = {
-  createUser
-};
-*/
-const { User } = require('../models');
-const Joi = require('joi');
-
-function validateUser(user) {
-  const schema = Joi.object({
-    username: Joi.string().alphanum().min(3).max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  });
-  return schema.validate(user);
-}
-
-const createUser = async (req, res) => {
-  try {
-    const { error } = validateUser(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const { username, email, password } = req.body;
-    const user = await User.create({ username, email, password });
-    res.status(201).json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send('Une erreur est survenue lors de la création de l\'utilisateur.');
   }
 }
 
